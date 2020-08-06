564e11842f7e2060e28d583ec83c035973751aa5
fourtyone11 <asleeppiano@outlook.com>
2020-08-06 20:30:57 +0300

    add weather icons

--- assets/js/weather.ts
+++ assets/js/weather.ts
@@ -1,7 +1,7 @@
 import { WEATHER_APPID } from './weather_app_id'
 import { getRandomInt } from './utilities'
 
-const apiAddr = (lat: string, lon: string) => `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&APPID=${WEATHER_APPID}`
+const apiAddr = (lat: string, lon: string) => `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&APPID=${WEATHER_APPID}`
 
 const defaultLatLon = [
   {
@@ -40,7 +40,7 @@ function fetchWeather(lat: string, lon: string) {
 
 function fetchCacheDecorator(this:any, wrapped: any) {
   return (...args: any[]) => {
-    const weather = localStorage.get('weather')
+    const weather = localStorage.getItem('weather')
     if(weather) {
       const weatherFetchTime = localStorage.getItem('weather_fetch_time')
       if(weatherFetchTime) {
@@ -62,6 +62,43 @@ function fetchCacheDecorator(this:any, wrapped: any) {
 
 const cachedWeather = fetchCacheDecorator(fetchWeather)
 
+function removeActiveIcon() {
+  const weatherElement = document.querySelector('.js-weather')
+  const activeIcon = weatherElement.querySelector('.svg-active')
+  if(!activeIcon) {
+    return
+  }
+  activeIcon.classList.remove('svg--active')
+}
+
+function renderWeatherIcon(weatherId: number) {
+  removeActiveIcon()
+
+  const weatherElement = document.querySelector('.js-weather')
+  if(weatherId >= 200 && weatherId < 300) {
+    const cloudIcon = weatherElement.querySelector('.svg-cloud')
+    cloudIcon.classList.add('svg--active');
+  } else if (weatherId >= 300 && weatherId < 400) {
+    const cloudIcon = weatherElement.querySelector('.svg-cloud')
+    cloudIcon.classList.add('svg--active');
+  } else if (weatherId >= 500 && weatherId < 600) {
+    const rainIcon = weatherElement.querySelector('.svg-rain-cloud')
+    rainIcon.classList.add('svg--active');
+  } else if (weatherId >= 600 && weatherId < 700) {
+    const snowIcon = weatherElement.querySelector('.svg-snowflake')
+    snowIcon.classList.add('svg--active')
+  } else if (weatherId === 800) {
+    const sunIcon = weatherElement.querySelector('.svg-sun')
+    sunIcon.classList.add('svg--active')
+  } else if (weatherId === 801) {
+    const cloudWithSunIcon = weatherElement.querySelector('.svg-cloud-sun')
+    cloudWithSunIcon.classList.add('svg--active')
+  } else {
+    const cloudIcon = weatherElement.querySelector('.svg-cloud')
+    cloudIcon.classList.add('svg--active')
+  }
+}
+
 function success(position: any) {
   console.log('pos', position)
   const latitude  = position.coords.latitude;
@@ -70,8 +107,12 @@ function success(position: any) {
   const status = document.querySelector<HTMLDivElement>('.c-weather__status');
 
   status.textContent = '';
-  cachedWeather(latitude, longitude)
-
+  const weather = cachedWeather(latitude, longitude)
+  weather.then(w => {
+    renderWeatherIcon(w.weather[0].id)
+    weatherButton.remove()
+    console.log('weather', w)
+  })
 }
 function error() {
   const status = document.querySelector<HTMLDivElement>('.c-weather__status');
@@ -79,7 +120,6 @@ function error() {
 }
 
 
-const weatherElement = document.querySelector('.js-weather')
 const weatherButton = document.querySelector('.js-weather-button')
 
 function handleWeatherButton() {
@@ -89,7 +129,12 @@ function handleWeatherButton() {
     navigator.geolocation.getCurrentPosition(success, error, { enableHighAccuracy: true });
   } else {
     const randomCity = getRandomInt(0, defaultLatLon.length)
-    cachedWeather(randomCity.lat, randomCity.lon)
+    const weather = cachedWeather(randomCity.lat, randomCity.lon)
+    weather.then(w => {
+      renderWeatherIcon(w.weather[0].id)
+      weatherButton.remove()
+      console.log('weather', w)
+    })
   }
 }
 
@@ -99,4 +144,9 @@ const randomCity = getRandomInt(0, defaultLatLon.length)
 
 if(window.location.host !== 'localhost:1313') {
   const weather = fetchWeather(randomCity.lat, randomCity.lon)
+  weather.then(w => {
+    renderWeatherIcon(w.weather[0].id)
+    weatherButton.remove()
+    console.log('weather', w)
+  })
 }
